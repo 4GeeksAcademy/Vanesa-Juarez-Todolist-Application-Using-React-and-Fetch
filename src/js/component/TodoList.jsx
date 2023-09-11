2bb157355b232fefbb457c4d215681d11ca49aeb
Vanesa Juarez Paris <131259155+vanesascode@users.noreply.github.com>
2023-09-11 14:45:46 +0000

    first stage style

--- src/js/component/TodoList.jsx
+++ src/js/component/TodoList.jsx
@@ -1,7 +1,6 @@
 import React, { useState, useEffect } from "react";
 
 function TodoList() {
-
   // Every task:
   const [todoInput, setTodoInput] = useState("");
 
@@ -10,7 +9,6 @@ function TodoList() {
 
   // Number of items to count them at the bottom:
   const [items, setItems] = useState(0);
-  
 
   // API REQUESTS: /////////////////////////////////////////////////////////
 
@@ -29,35 +27,30 @@ function TodoList() {
   //   };
 
   //   fetch("https://playground.4geeks.com/apis/fake/todos/user/vanesa", requestOptions)
-  //     .then( response =>  response.json()) 
+  //     .then( response =>  response.json())
   //     .then(result => setData(result))
   // }, []);
-
 
   //////////////////////////////////////////////////////////////////////////////////////
 
   // GET THE DATA ///////////////////////////////////////////////////////////////////
 
- 
+  // requestsOptions are not necessary, by default it's 'GET':
 
-    // requestsOptions are not necessary, by default it's 'GET':
+  // const requestOptions = {
+  //   method: 'GET',
+  // };
 
-    // const requestOptions = {
-    //   method: 'GET',
-    // };
-
-    const fetchTodos = () => {
-      fetch("https://playground.4geeks.com/apis/fake/todos/user/vanesascode")
-      .then(response => response.json()) 
-      .then(result => setData(result))
+  const fetchTodos = () => {
+    fetch("https://playground.4geeks.com/apis/fake/todos/user/vanesascode")
+      .then((response) => response.json())
+      .then((result) => setData(result))
       // .then(data => console.log(data))
-      .catch(error => console.log('Error:', error));
-    }
+      .catch((error) => console.log("Error:", error));
+  };
 
   useEffect(() => {
-    
-    fetchTodos()
-
+    fetchTodos();
   }, []);
 
   ///////////////////////////////////////////////////////////
@@ -66,55 +59,55 @@ function TodoList() {
 
   const addTodo = () => {
     if (todoInput.trim() !== "") {
-      setData([...data, {label:todoInput, done:false}]);
+      setData([...data, { label: todoInput, done: false }]);
       setTodoInput("");
     }
-  }
+  };
 
   ////////////////////////////////////////////////////////////
 
   // UPDATE DATA //
 
   useEffect(() => {
-
-   const requestOptions = {
-      method: 'PUT',
+    const requestOptions = {
+      method: "PUT",
       body: JSON.stringify(data),
-      redirect: 'follow',
+      redirect: "follow",
       headers: {
         "Content-Type": "application/json",
       },
     };
 
-    fetch("https://playground.4geeks.com/apis/fake/todos/user/vanesascode", requestOptions)
-      .then(data => {
+    fetch(
+      "https://playground.4geeks.com/apis/fake/todos/user/vanesascode",
+      requestOptions
+    )
+      .then((data) => {
         if (data.result === "ok") {
           fetchTodos();
         } else {
           console.log("Failed to create new todo");
         }
       })
-      .catch(error => {
+      .catch((error) => {
         console.error("Error:", error);
       });
-
   }, [data]);
-
 
   ///////////////////////////////////////////////////////////////////////////////
 
-    // DELETE TASKS //
+  // DELETE TASKS //
 
-    const deleteTodo = (task) => {
-      console.log(task) 
-      task.done = true
-      const newData = data.filter(e => e.done != true)
-      setData(newData)
-    }
+  const deleteTodo = (task) => {
+    console.log(task);
+    task.done = true;
+    const newData = data.filter((e) => e.done != true);
+    setData(newData);
+  };
 
   /////////////////////////////////////////////////////////////////////////////////
 
-  //Counter of number of tasks: 
+  //Counter of number of tasks:
 
   useEffect(() => {
     const itemsLeft = data.length;
@@ -123,56 +116,66 @@ function TodoList() {
 
   /////////////////////////////////////////////////////////////////////////////////
 
+  const [inputOn, setInputOn] = useState(false);
+
   return (
-    <div className="d-flex justify-content-center">
-      <div className="col-md-4">
-        <div className="layer1 d-flex justify-content-start flex-column border-0 shadow z-3">
-          
-          {/* input*/}
+    <>
+      <div className=" d-flex justify-content-start flex-column border-0 z-3 mt-5">
+        {/* input*/}
 
-          <input
-            type="text"
-            value={todoInput}
-            onChange={(e) => setTodoInput(e.target.value)}
-            placeholder="What needs to be done?"
-            onKeyDown={(e) => {
-              if (e.key === "Enter") {
-                addTodo();
-              }
-            }}
-            className="border-0 px-5 py-3 input fs-2"
-          />
+        <input
+          type="text"
+          value={todoInput}
+          onChange={(e) => setTodoInput(e.target.value)}
+          placeholder="What needs to be done?"
+          onKeyDown={(e) => {
+            if (e.key === "Enter") {
+              addTodo();
+            }
+          }}
+          onFocus={() => setInputOn(true)}
+          onBlur={() => setInputOn(false)}
+          className={`mb-5 border-0 px-md-5 px-4 py-3 fs-2  ${
+            inputOn ? "input-on" : "input-off"
+          }`}
+        />
 
+        {/* tasks + counter*/}
+        <div className="list p-3">
           {/* tasks*/}
+          {data &&
+            data.map((task, index) => (
+              <div
+                key={index}
+                className=" w-full px-md-4 px-1 py-3 d-flex justify-content-between fs-2"
+              >
+                <div className="me-5">{task.label}</div>
 
-          {data.map((task, index) => (
-            <div
-              key={index}
-              className="bg-white item-box border-top w-full px-5 py-3 d-flex justify-content-between fs-2"
-              onClick={() => deleteTodo(task)}
-            >
-             
-              <div className="me-5">{task.label}</div>
+                {/* erase button*/}
 
-          {/* erase button*/}
-
-              <div>
-                <button>X</button>
+                <div>
+                  <button
+                    className="border-0 px-3"
+                    onClick={() => deleteTodo(task)}
+                  >
+                    X
+                  </button>
+                </div>
               </div>
-            </div>
-          ))}
+            ))}
+        </div>
+        {/* counter*/}
 
-          {/* number of tasks in the list*/}
-
-          <div className="d-flex justify-content-end border-top bg-white">
-            <div className="py-2 px-5 fs-5 bg-white">
-              {items} {items === 1 ? "item" : "items"} left
-            </div>
+        <div className="d-flex justify-content-center mt-4  ">
+          <div>
+            <h2 className="fw-bolder fs-2 ">
+              {" "}
+              {items} {items === 1 ? "Item" : "Items"} Left
+            </h2>
           </div>
-
         </div>
       </div>
-    </div>
+    </>
   );
 }
 
--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,41 +1,47 @@
-import React from 'react';
-// import FetchPromise from './FetchPromise';
-// import FetchAsync from './FetchAsync';
-import TodoList from './TodoList';
-import OriginalTodoList from './OriginalTodoList';
-
+import React, { useState } from "react";
+import TodoList from "./TodoList";
 
 function Home() {
-  
+  const [titleOn, setTitleOn] = useState(false);
+
   return (
     <>
-   
- {/* TODO LIST */}
-    <div className="text-center mb-5">
-      <h1 className="text-grey my-4 title ">Todos with Fetch and API</h1>
-      <div>
-        <TodoList />
+      <div className="text-center bg-div d-flex justify-content-center px-2">
+        <div
+          className={`my-4 bg-box px-md-5 px-3 pt-5 col-12 col-lg-6 ${
+            titleOn ? " pb-4" : " pb-5"
+          }`}
+        >
+          <div className="d-flex align-items-center flex-column flex-md-row ">
+            {/*TITLE */}
+
+            <h1
+              className={`flex-fill fw-bolder me-0 me-md-5 mb-4 mb-md-0 ${
+                titleOn ? "bg-title-on " : "bg-title-off "
+              } fs-1 p-5 text-center`}
+              onClick={() => setTitleOn(!titleOn)}
+            >
+              Click to Open the Todolist App Using React and Fetch
+            </h1>
+
+            {/*DARK MODE CHECKBOX */}
+
+            <div className="d-flex flex-column align-items-center">
+              <div className="container ">
+                <input type="checkbox" class="checkbox" id="checkbox" />
+                <label className="switch" for="checkbox">
+                  <span className="slider"></span>
+                </label>
+              </div>
+              {/*dark mode title*/}
+              <h1 className="fs-2 mt-3">Dark Mode</h1>
+            </div>
+          </div>
+          {/*TODOLIST */}
+
+          {titleOn && <TodoList />}
+        </div>
       </div>
-    </div>
-
-    {/* ORIGINAL TODO LIST */}
-    
-    <div className="text-center mt-5">
-    <h1 className="text-grey my-4 title ">Original Todos List</h1>
-      <OriginalTodoList />
-    </div>
-
-
-    {/*EXAMPLES OF FETCHING DATA*/}
-    
-    <div className='bg-warning mt-5'>
-      {/* <FetchAsync /> */}
-    </div>
-
-   <div className='bg-success'>
-      {/* <FetchPromise /> */}
-    </div>
-
     </>
   );
 }
